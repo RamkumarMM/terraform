a826c64c9418e0e1c32334e81ca80d3befa98965
Ramkumar MM <ramkumarmm@gmail.com>
2020-08-26 07:46:01 +0530

    Modified tiny network range for learning purpose

--- dev/main.tf
+++ dev/main.tf
@@ -1,12 +1,13 @@
 module "create_vpc" {
   source = "../modules/vpc"
+  environment = "dev"
   vpc_name = "dev-vpc"
-  vpc_network = "10.16.0.0/16"
+  vpc_cidr = "192.168.1.0/26"
   vpc_tenancy = "default"
   vpc_id = "${module.create_vpc.vpc_id}"
   igw_name = "dev-vpc-igw"
-  public_subnet = [ "10.16.1.0/24"]
+  public_subnet = [ "192.168.1.0/28"]
   public_subnet_az = [ "ap-south-1a" ]
-  private_subnet = [ "10.16.2.0/24", "10.16.3.0/24" ]
+  private_subnet = [ "192.168.1.16/28", "192.168.1.32/28" ]
   private_subnet_az = [ "ap-south-1b", "ap-south-1c" ]
 }
